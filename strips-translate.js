758add40b7f95d1a939b7e610c1852a8c93d9758
Lars Mats <sweesoft@gmail.com>
2020-12-13 22:21:59 +0100

    Typo in metadata

--- strips-translate.js
+++ strips-translate.js
@@ -118,7 +118,7 @@ const STRIPS_REPORTS = {
     CapacitancePadReport:   { reportbit: 20, sensors: STRIPS_SENSOR.CAP,                      coding: UNSIGN2, channel: 113, unit:''    },
     CapacitanceEndReport:   { reportbit: 21, sensors: STRIPS_SENSOR.CAP,                      coding: UNSIGN2, channel: 114, unit:''    },
     UserSwitch1Alarm:       { reportbit: 22, sensors: STRIPS_SENSOR.TAMPER,                   coding: DIGITAL, channel: 16,  unit:''    },
-    DoorCountReport:        { reportbit: 23, sensors: STRIPS_SENSOR.TAMPER,                   coding: UNSIGN2, channel: 17,  unit:''    },
+    DoorCountReport:        { reportbit: 23, sensors: STRIPS_SENSOR.DOOR,                     coding: UNSIGN2, channel: 17,  unit:''    },
     PresenceReport:         { reportbit: 24, sensors: STRIPS_SENSOR.PROX,                     coding: DIGITAL, channel: 18,  unit:''    },
     IRProximityReport:      { reportbit: 25, sensors: STRIPS_SENSOR.PROX,                     coding: UNSIGN2, channel: 19,  unit:''    },
     IRCloseProximityReport: { reportbit: 26, sensors: STRIPS_SENSOR.PROX,                     coding: UNSIGN2, channel: 20,  unit:''    },
